2e05e5d8e6efaa6690cb92cd8312da7b42132aac
cubercsl <2014cais01@gmail.com>
2021-01-05 19:56:30 +0800

    update demo

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
 var dom = document.getElementById('main')
 var chart = echarts.init(dom)
 
-var options = {
-    baseOption: {
-        title: {
-            text: '中国魔方比赛参赛人次史'
-        },
-        visualMap: {
-            max: 1000,
-            calculable: true,
-        },
-        tooltip: {
-        },
-        series: [{
-            type: 'map',
-            name: '累计参赛人次',
-            map: 'china',
-        }],
-
-    }
-}
-chart.setOption(options)
-
+chart.showLoading();
 load_data()
--- load.js
+++ load.js
@@ -1,24 +1,88 @@
 function load_data() {
     $.getJSON('data/data.json', (data) => {
-        options.options = data.map(item => {
-            return {
-                series: {
-                    data: item.data
-                },
-                title: {
-                    subtext: item.date
+        options = {
+            title: [{
+                text: '',
+                textAlign: 'center',
+                left: '63%',
+                top: '55%',
+                textStyle: {
+                    fontSize: 100
                 }
+            }, {
+                text: '中国魔方比赛参赛人次变化史',
+                subtext: '数据来源：世界魔方协会',
+                sublink: 'https://worldcubeassociation.org/'
+            }],
+            visualMap: {
+                max: 1000,
+                calculable: true,
+            },
+            tooltip: {
+            },
+
+            grid: [{
+                left: '2%',
+                width: '20%',
+                height: '30%',
+                containLabel: true
+            }],
+            xAxis: {
+                type: 'value',
+                max: 'dataMax',
+            },
+            yAxis: {
+                type: 'category',
+                inverse: true
+            },
+            series: [
+                {
+                    id: 0,
+                    type: 'bar',
+                    name: '累计参赛人次',
+                    label: {
+                        show: true,
+                        position: 'right',
+                        valueAnimation: true
+                    }
+                }, {
+                    type: 'map',
+                    name: '累计参赛人次',
+                    map: 'china'
+                }],
+            options: data.map(item => {
+                return {
+                    series: [{
+                        data: item.data.map(item => [item.value, item.name, item.value])
+                    }, {
+                        data: item.data
+                    }],
+                    title: {
+                        text: item.date,
+                        textAlign: 'center',
+                        left: '20%',
+                        top: '75%',
+                        textStyle: {
+                            fontSize: 60
+                        }
+                    },
+                    yAxis: {
+                        max: Math.min(item.data.length - 1, 6),
+                        data: item.data.sort((a, b) => b.value - a.value).map(item => item.name)
+                    }
+                }
+            }),
+            timeline: {
+                axisType: 'category',
+                autoPlay: true,
+                playInterval: 1000,
+                loop: false,
+                left: '10%',
+                right: '10%',
+                data: data.map(item => item.date),
             }
-        })
-        options.timeline =  {
-            axisType: 'category',
-            autoPlay: true,
-            data: data.map(item => item.date),
-            playInterval: 5,
-            loop: false,
-            left: '10%',
-            right: '10%'
         }
+        chart.hideLoading()
         chart.setOption(options)
     })
 }
